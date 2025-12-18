12acd901a99436bb60ca441913be4307db7b4c85
Aero Blue <48103037+Aero-Blue@users.noreply.github.com>
2025-12-17 22:18:36 -0600

    Create basic layout (header + footer)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,23 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'AeroBlue.dev'
+export interface NavLink {
+  name: string
+  href: string
+}
+export interface SocialLink {
+  name: string
+  href: string
+  icon: string
+}
+
+export const SITE_TITLE = 'AeroBlue'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_NAV_LINKS: NavLink[] = [
+  { name: 'Blog', href: '/blog' },
+  { name: 'Portfolio', href: '/portfolio' },
+  { name: 'About', href: '/about' },
+]
+export const SITE_SOCIAL_LINKS: SocialLink[] = [
+  { name: 'GitHub', href: 'https://github.com/Aero-Blue', icon: 'lucide:github' },
+]
