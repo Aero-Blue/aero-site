3cc4cf06487fc863f893f4ce245c2c0c8a38e673
Aero Blue <48103037+Aero-Blue@users.noreply.github.com>
2025-12-21 17:39:43 -0600

    Tweak formatting / linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,21 @@
 import eslintPluginAstro from 'eslint-plugin-astro'
 import tsParser from '@typescript-eslint/parser'
 import tsPlugin from '@typescript-eslint/eslint-plugin'
-
+import eslintConfigPrettier from 'eslint-config-prettier'
 export default [
   ...eslintPluginAstro.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,tsx}'],
-    languageOptions: { parser: tsParser },
     plugins: { '@typescript-eslint': tsPlugin },
+    languageOptions: { parser: tsParser },
     rules: { ...tsPlugin.configs.recommended.rules },
   },
   {
     files: ['**/*.astro'],
     plugins: { '@typescript-eslint': tsPlugin },
+    rules: { ...tsPlugin.configs.recommended.rules },
   },
+  eslintConfigPrettier,
   {
     ignores: ['dist/', '.astro/', 'pnpm-lock.yaml', 'package-lock.json'],
   },
