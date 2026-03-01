1e08c4e3fa8e2fc476d66692b3d77f264ef99b2e
Aero Blue <48103037+Aero-Blue@users.noreply.github.com>
2026-02-28 18:36:55 -0600

    Add presistent theme toggle for Daisy and Expressive Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,28 @@ import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
-
 import icon from 'astro-icon'
-import { remarkReadingTime } from './utils/remark-reading-time'
+import { remarkReadingTime } from './src/utils/remark-reading-time'
+import remarkToc from 'remark-toc'
+import remarkDirective from 'remark-directive'
+import astroExpressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aeroblue.dev',
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    astroExpressiveCode({
+      themes: ['rose-pine-moon', 'rose-pine-dawn'],
+      themeCssSelector: (theme) => {
+        return theme.name === 'rose-pine-moon' ? '[data-theme="dark"]' : '[data-theme="light"]'
+      },
+    }),
+    mdx(),
+    sitemap(),
+    icon(),
+  ],
   markdown: {
-    shikiConfig: {
-      theme: 'rose-pine-moon',
-    },
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkDirective, remarkReadingTime, remarkToc],
   },
   vite: {
     plugins: [tailwindcss()],
